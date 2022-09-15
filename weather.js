7ef0daa300018e6285aa321de8334eaf8a4c6146
vvjs-22 <s.jayeshchennai@gmail.com>
2022-09-15 18:36:36 +0530

    latitude longitude measurements

--- weather.js
+++ weather.js
@@ -1,8 +1,7 @@
 const showPosition = async (position) => {
     const lat = position.coords.latitude
     const lng = position.coords.longitude
-    const place = await getPlaceName(lat, lng)
-    getWeather(place)
+    getWeather(lat, lng)
 }
   
 const showError = (error) => {
@@ -28,26 +27,7 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, showError);
 }
 
-const getPlaceName = async (lat, lng) => {
-    const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=pk.eyJ1IjoianN3aXRoanMiLCJhIjoiY2t2bjl2cXE3OW81MjJuczdnbTdoa2tnNyJ9.vyi3KY_FqFWSAdpuR-qTzQ`)
-    const data = await response.json()
-
-    const district = data.features.filter(item => {
-        return item.id.includes("district")
-    })
-    const place = data.features.filter(item => {
-        return item.id.includes("place")
-    })
-    if (district.length > 0) {
-        return district[0].text
-    } else if (place.length > 0) {
-        return place[0].text
-    } else {
-        return false
-    }
-}
-
-const getWeather = (place) => {
+const getWeather = (lat, lng) => {
     const box = document.querySelector(".weather-wrapper");
     const wrapper = document.querySelector(".days-wrapper");
     const unitChanger = document.querySelector("#unit");
@@ -55,57 +35,48 @@ const getWeather = (place) => {
     box.classList.add("hide")
     document.querySelector(".place").innerText = "";
     document.querySelector(".loading").classList.remove("hide");
-        if (place) {
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=10d535d8dad64606479a529972f16544&units=metric`)
-                .then(response => response.json())
-                .then(data => {
-                    const set = new Set()
-                    const unique = data.list.filter(item => {
-                    const date = item.dt_txt.slice(8,10)
-                    const isDuplicate = set.has(date)
-                    set.add(date)
-                    if (!isDuplicate) {
-                        return true
-                    }
-                    return false
-                    })
-                    document.querySelector(".loading").classList.add("hide");
-                    document.querySelector(".place").innerText = place;
-                    wrapper.innerHTML = "";
-                    unitChanger.classList.contains("hideV") && unitChanger.classList.remove("hideV");
-                    box.classList.remove("hide");
-                    wrapper.classList.remove("hide");
-                    notFound.classList.add("hide");
-                    unique.map(item => {
-                        const date = String(new Date(item.dt_txt.slice(0,10))).slice(4,15);
-                        wrapper.innerHTML += `<div class="day-weather">
-                        <img src="http://openweathermap.org/img/w/${item.weather[0].icon}.png" alt="Clouds">
-                        <div class="date">${date}</div>
-                        <div class="temp"><span class="temp-value">${item.main.temp.toFixed(2)}</span> <span class="temp-unit">°C</span></div>
-                        <div class="type">${item.weather[0].main}</div>
-                        </div>`
-                        });
-                        if (unitChanger.value === "Fahrenheit") {
-                            changeUnit("Fahrenheit")
-                        }
-                })
-                .catch (() => {
-                    document.querySelector(".loading").classList.add("hide");
-                    document.querySelector(".place").innerText = "";
-                    box.classList.remove("hide");
-                    wrapper.classList.add("hide");
-                    unitChanger.classList.add("hideV");
-                    notFound.classList.remove("hide");
-                })
-        } else {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=10d535d8dad64606479a529972f16544&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            const set = new Set()
+            const unique = data.list.filter(item => {
+            const date = item.dt_txt.slice(8,10)
+            const isDuplicate = set.has(date)
+            set.add(date)
+            if (!isDuplicate) {
+                return true
+            }
+            return false
+            })
+            console.log(data)
+            document.querySelector(".loading").classList.add("hide");
+            document.querySelector(".place").innerText = data.city.name || `latitude ${lat.toFixed(3)} / longitude: ${lng.toFixed(3)}`;
+            wrapper.innerHTML = "";
+            unitChanger.classList.contains("hideV") && unitChanger.classList.remove("hideV");
+            box.classList.remove("hide");
+            wrapper.classList.remove("hide");
+            notFound.classList.add("hide");
+            unique.map(item => {
+                const date = String(new Date(item.dt_txt.slice(0,10))).slice(4,15);
+                wrapper.innerHTML += `<div class="day-weather">
+                <img src="http://openweathermap.org/img/w/${item.weather[0].icon}.png" alt="Clouds">
+                <div class="date">${date}</div>
+                <div class="temp"><span class="temp-value">${item.main.temp.toFixed(2)}</span> <span class="temp-unit">°C</span></div>
+                <div class="type">${item.weather[0].main}</div>
+                </div>`
+                });
+                if (unitChanger.value === "Fahrenheit") {
+                    changeUnit("Fahrenheit")
+                }
+        })
+        .catch (() => {
             document.querySelector(".loading").classList.add("hide");
             document.querySelector(".place").innerText = "";
             box.classList.remove("hide");
             wrapper.classList.add("hide");
             unitChanger.classList.add("hideV");
             notFound.classList.remove("hide");
-        }
-        
+        })
 }
 
 const changeUnit = (unit) => {
@@ -135,16 +106,14 @@ const changeUnit = (unit) => {
 
 mapboxgl.accessToken = 'pk.eyJ1IjoianN3aXRoanMiLCJhIjoiY2t2bjl2cXE3OW81MjJuczdnbTdoa2tnNyJ9.vyi3KY_FqFWSAdpuR-qTzQ';
 const map = new mapboxgl.Map({
-        container: 'map', // container id
-        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [78.23539967204061, 11.458216832796865], // starting position
-        zoom: 6 // starting zoom
+        center: [78.23539967204061, 11.458216832796865],
+        zoom: 6
 });
 
 map.on('click', async (e) => {
     const lat = e.lngLat.wrap().lat
     const lng = e.lngLat.wrap().lng
-    const place = await getPlaceName(lat, lng)
-    getWeather(place)
+    getWeather(lat, lng)
 });
